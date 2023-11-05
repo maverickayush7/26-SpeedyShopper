7dfe310ddc91d4bd9bd5073d7dbd8a286063549a
Arunabh13prt <arunabh.barooah04@gmail.com>
2023-11-05 10:19:59 +0530

    Added Connection to Database

--- src/Components/RowData.js
+++ src/Components/RowData.js
@@ -1,86 +1,78 @@
 export const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 1024 },
-      items: 5,
-      slidesToSlide: 2,
-    },
-    desktop: {
-      breakpoint: { max: 1024, min: 800 },
-      items: 4,
-    },
-    tablet: {
-      breakpoint: { max: 800, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
-  
-  export const productData = [
-    {
-      id: 1,
-      imageurl:
-        "https://thumbs.dreamstime.com/b/groceries-paper-bag-vector-illustration-97077851.jpg",
-      name: "Item Name",
-      price: "Rs 100",
-      description: "Some text about the product..",
-    },
-    {
-      id: 2,
-      imageurl:"https://thumbs.dreamstime.com/b/groceries-paper-bag-vector-illustration-97077851.jpg",
-      name: "Item Name",
-      price: "Rs 100",
-      description: "Some text about the product..",
-    },
-    {
-      id: 3,
-      imageurl:
-        "https://thumbs.dreamstime.com/b/groceries-paper-bag-vector-illustration-97077851.jpg",
-      name: "Item Name",
-      price: "Rs 100",
-      description: "Some text about the product..",
-    },
-    {
-      id: 4,
-      imageurl:
-        "https://thumbs.dreamstime.com/b/groceries-paper-bag-vector-illustration-97077851.jpg",
-      name: "Item Name",
-      price: "Rs 100",
-      description: "Some text about the product..",
-    },
-    {
-      id: 5,
-      imageurl:
-        "https://thumbs.dreamstime.com/b/groceries-paper-bag-vector-illustration-97077851.jpg",
-      name: "Item Name",
-      price: "Rs 100",
-      description: "Some text about the product..",
-    },
-    {
-      id: 6,
-      imageurl:
-        "https://thumbs.dreamstime.com/b/groceries-paper-bag-vector-illustration-97077851.jpg",
-      name: "Item Name",
-      price: "$Rs 100",
-      description: "Some text about the product..",
-    },
-    {
-      id: 7,
-      imageurl:
-        "https://thumbs.dreamstime.com/b/groceries-paper-bag-vector-illustration-97077851.jpg",
-      name: "Item Name",
-      price: "Rs 100",
-      description: "Some text about the product..",
-    },
-    {
-      id: 8,
-      imageurl:
-        "https://thumbs.dreamstime.com/b/groceries-paper-bag-vector-illustration-97077851.jpg",
-      name:"Item Name",
-      price: "Rs 100",
-      description: "Some text about the product..",
-    },
-  ];
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 1024 },
+    items: 5,
+    slidesToSlide: 2,
+  },
+  desktop: {
+    breakpoint: { max: 1024, min: 800 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 800, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+export const productData = [
+  {
+    id: 1,
+    imageurl: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/197975a.jpg?ts=1691659438",
+    name: "Orange",
+    price: "₹59",
+    description: "Some text about the product..",
+  },
+  {
+    id: 2,
+    imageurl: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/391306a.jpg?ts=1690814339",
+    name: "Onion",
+    price: "₹125",
+    description: "Some text about the product..",
+  },
+  {
+    id: 3,
+    imageurl: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/382144a.jpg?ts=1695733974",
+    name: "Baby Apple Shimla",
+    price: "₹140",
+    description: "Some text about the product..",
+  },
+  {
+    id: 4,
+    imageurl: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/4926a.jpg?ts=1690814792",
+    name: "Potato",
+    price: "₹34",
+    description: "Some text about the product..",
+  },
+  {
+    id: 5,
+    imageurl: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/368037a.jpg?ts=1691671171",
+    name: "Carrot",
+    price: "₹11",
+    description: "Some text about the product..",
+  },
+  {
+    id: 6,
+    imageurl: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/445675a.jpg?ts=1690813629",
+    name: "Tomato",
+    price: "₹30",
+    description: "Some text about the product..",
+  },
+  {
+    id: 7,
+    imageurl: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/3874a.jpg?ts=1697346451",
+    name: "Banana",
+    price: "₹17",
+    description: "Some text about the product..",
+  },
+  {
+    id: 8,
+    imageurl: "https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/images/products/sliding_image/10088a.jpg?ts=1690813243",
+    name: "Cucumber",
+    price: "₹16",
+    description: "Some text about the product..",
+  },
+];
